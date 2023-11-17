7057c7525905c59fd7f76505a8bac22ef891ba19
Miguel Rivas <rivashernandezm@roverkids.org>
2023-11-17 14:01:43 -0500

    Lesson 1.3 - High Speed Chase

--- Prototype1/Assets/Scripts/PlayerController.cs
+++ Prototype1/Assets/Scripts/PlayerController.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
+
 {
+    public float speed = 5.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //We'll move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
